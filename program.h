82dfcc93ec1df4470fbfaed06cb2101ad9356ed3
rafaaufa <vicdhan@mhs.usk.ac.id>
2026-05-20 15:27:54 +0700

    tambah fungsi sort

--- program.h
+++ program.h
@@ -6,3 +6,6 @@ void insertion_sort(int arr[], int n);
 void selection_sort(int arr[], int n);
 
 //sorting advaced
+void merge_sort(int arr[], int l, int r);
+void quick_sort(int arr[], int low, int high);
+void shell_sort(int arr[], int n);
