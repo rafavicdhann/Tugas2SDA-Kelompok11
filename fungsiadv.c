14fbfaff072f8d466fd7466b761c10f725b3bedc
riskimaulidan02 <riski346@mhs.usk.ac.id>
2026-05-28 13:26:15 +0700

    melengkapi fungsiadv

--- fungsiadv.c
+++ fungsiadv.c
@@ -48,3 +48,60 @@ void merge_sort(char arr[][MAX_PANJANG_KATA], int l, int r){
         merge(arr, l, m, r);
     }
 }
+
+void swap(char a[], char b[]) {
+    char temp[MAX_PANJANG_KATA];
+    strcpy(temp, a);
+    strcpy(a, b);
+    strcpy(b, temp);
+}
+
+int partition(char arr[][MAX_PANJANG_KATA], int low, int high) {
+    char pivot[MAX_PANJANG_KATA];
+    strcpy(pivot, arr[high]);
+    int i = low - 1;
+    for (int j = low; j <= high - 1; j++) {
+        if (strcmp(arr[j], pivot) < 0) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quick_sort(char arr[][MAX_PANJANG_KATA], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quick_sort(arr, low, pi - 1);
+        quick_sort(arr, pi + 1, high);
+    }
+}
+
+void shell_sort(char arr[][MAX_PANJANG_KATA], int n) {
+    int j;
+    char temp[MAX_PANJANG_KATA];
+    for (int interval = n / 2; interval > 0; interval /= 2) {
+        for (int i = interval; i < n; i++) {
+            strcpy(temp, arr[i]);
+            for (j = i; j >= interval && strcmp(arr[j - interval], temp) > 0; j -= interval) {
+                strcpy(arr[j], arr[j - interval]);
+            }
+            strcpy(arr[j], temp);
+        }
+    }
+}
+
+int read_file(const char *filename, char data[][MAX_PANJANG_KATA], int max_data) {
+    int count = 0;
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        fprintf(stderr, "gagal membuka file %s\n", filename);
+        return 0;
+    }
+    while (count < max_data && fscanf(file, "%s", data[count]) == 1) {
+        count++;
+    }
+    fclose(file);
+    return count;
+}
